906592db039f6831f87c0e25b71ba3883566770b
superhegsy <csaba.hegedus@elinuxmail.org>
2026-03-07 21:36:40 +0100

    fix urls remove missing api endpoints

--- vpnreport/urls.py
+++ vpnreport/urls.py
@@ -1,31 +1,17 @@
-"""
-URL configuration for vpnreport project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
 from vpn import views
 
 
 urlpatterns = [
+
+    # ADMIN
     path('admin/', admin.site.urls),
 
     # DASHBOARD
     path('', views.dashboard, name='dashboard'),
 
-    # REPORT PAGES
+    # REPORTS
     path('report/daily/', views.report_daily, name='report_daily'),
     path('report/weekly/', views.report_weekly, name='report_weekly'),
     path('report/monthly/', views.report_monthly, name='report_monthly'),
@@ -33,11 +19,10 @@ urlpatterns = [
     # USER HISTORY
     path('report/user/<str:username>/', views.user_history, name='user_history'),
 
-    # API
-    path('api/vpn-locations/', views.vpn_locations),
-    path('api/dashboard-stats/', views.dashboard_stats),
-    path('api/active-vpn/', views.active_vpn_sessions),
-
     # PDF REPORT
     path('report/pdf/<str:period>/', views.report_pdf, name='report_pdf'),
+
+    # LIVE DASHBOARD API
+    path('api/live-dashboard/', views.live_dashboard, name='live_dashboard'),
+
 ]
