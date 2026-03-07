e53aae58e66b996d4f850035b63b765a81cd8694
superhegsy <csaba.hegedus@elinuxmail.org>
2026-03-07 19:17:42 +0100

    login system settings

--- vpnreport/settings.py
+++ vpnreport/settings.py
@@ -104,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'hu'
 
 TIME_ZONE = 'Europe/Budapest'
 
@@ -124,3 +124,11 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
+
+# ======================================================
+# AUTH LOGIN
+# ======================================================
+
+LOGIN_URL = "/login/"
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/login/"
